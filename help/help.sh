a6d0af72da766bfffbefe3f47f424cd6f19c23c3
Tarna <158784522+TarnaWijaya@users.noreply.github.com>
2025-02-20 12:36:34 +0700

    update help/help.sh

---
+++ help/help.sh
@@ -0,0 +1,18 @@
+echo "
+____  ______________                        _____  .___ 
+\   \/  /\__    ___/____ _______________   /  _  \ |   |
+ \     /   |    |  \__  \\_  __ \___   /  /  /_\  \|   |
+ /     \   |    |   / __ \|  | \//    /  /    |    \   |
+/___/\  \  |____|  (____  /__|  /_____ \ \____|__  /___|
+      \_/               \/            \/         \/     
+    [ ☞⁠￣⁠ᴥ⁠￣⁠☞ Selamat Datan dipusat bantuan Module XTarzAI]
+    
+  anda bisa bergabung digrup
+    Telegram Grup: https://t.me/TarnaWijaya_grup
+    WhatsApp Grup: https://chat.whatsapp.com/Gomu4BhzluT3gaXRHmNs4n
+
+  Jika ada masalah tentang module ini langsung pm
+    Telegram: @TarnaWijaya
+    WhatsApp: +62 851-3553-0429
+    
+    Terimakasih sudah memakai Module ini❤️"
